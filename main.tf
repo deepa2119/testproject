e09e1a1c64697028fe994fc0e5a22e2c270ab08d
pradeep <deepa2119@live.com>
2023-08-17 17:15:43 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myin" {
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Test-Instance"
+  }
+}
